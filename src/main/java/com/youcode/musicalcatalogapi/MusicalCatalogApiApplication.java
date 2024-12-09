ff5fced00e8f4ccbc0abd7d8cdad614c3c851f9e
WailBoukhari1 <boukahri.wail002@gmail.com>
2024-12-09 14:35:57 +0100

    inint

---
+++ src/main/java/com/youcode/musicalcatalogapi/MusicalCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.musicalcatalogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalCatalogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicalCatalogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youcode/musicalcatalogapi/MusicalCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.musicalcatalogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalCatalogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
