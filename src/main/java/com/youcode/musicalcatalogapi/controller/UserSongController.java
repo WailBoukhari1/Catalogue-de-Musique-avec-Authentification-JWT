3b5824eb4472eaa65311f61431160a5f2a1b646f
WailBoukhari1 <boukahri.wail002@gmail.com>
2024-12-11 09:22:30 +0100

    MCA-14 song discovery and listing is done

--- src/main/java/com/youcode/musicalcatalogapi/controller/UserSongController.java
+++ src/main/java/com/youcode/musicalcatalogapi/controller/UserSongController.java
@@ -1,7 +1,16 @@
 package com.youcode.musicalcatalogapi.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.youcode.musicalcatalogapi.model.Song;
+import com.youcode.musicalcatalogapi.service.SongService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,5 +18,26 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/user/songs")
 @RequiredArgsConstructor
 public class UserSongController {
+    private final SongService songService;
 
+    @GetMapping
+    public ResponseEntity<Page<Song>> getAllSongs(Pageable pageable) {
+        return ResponseEntity.ok(songService.getAllSongs(pageable));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<Song>> searchByTitle(
+            @RequestParam String titre,
+            Pageable pageable
+    ) {
+        return ResponseEntity.ok(songService.searchByTitle(titre, pageable));
+    }
+
+    @GetMapping("/album/{albumId}")
+    public ResponseEntity<Page<Song>> getSongsByAlbum(
+            @PathVariable String albumId,
+            Pageable pageable
+    ) {
+        return ResponseEntity.ok(songService.getSongsByAlbum(albumId, pageable));
+    }
 } 
